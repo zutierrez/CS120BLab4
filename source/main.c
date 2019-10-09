ae6b8a0cf777ef7587a4ae8afcdd484320741bc2
Zion Gutierrez <zguti001@wch136-15.cs.ucr.edu>
2019-10-09 12:20:53 -0700

    Exercise 1 & Tests

--- source/main.c
+++ source/main.c
@@ -1,23 +1,76 @@
-/*	Author: zguti001
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
+/*	Author: Zion Gutierrez, zguti001
+ *      Partner(s) Name: none
+ *	Lab Section: 023
+ *	Assignment: Lab #4 Exercise #1
+ *	Exercise Description: LED & BUTTON 
  *
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
+#define inputA PINA
+
+enum SLED_States { SLED_Start, SLED_ONOF , SLED_SWITCH } SLED_State;
+
+void SwitchLED(SLED_State)
+{
+	switch(SLED_State) {      //TRANSITIONS
+		
+	    case SLED_Start:      //initial transition
+		SLED_State = SLED_ONOF;
+		break;
+	    
+	    case SLED_ONOF:
+	       if(inputA == 0x00){		
+		SLED_State = SLED_ONOF;
+	       }
+	       else if(inputA == 0x01){		
+		SLED_State = SLED_SWITCH;
+	       }
+	       break;
+
+	    case SLED_SWITCH:
+	       if(inputA == 0x00){		
+		SLED_State = SLED_SWITCH;
+	       }
+	       else if(inputA == 0x01){		
+		SLED_State = SLED_ONOF;
+	       }
+	       break;
+	    
+	    default:
+	        SLED_State = SLED_Start;
+	       break;
+	}
+	switch(SLED_State) {      //ACTIONS
+	    
+	    case SLED_ONOF:		
+		PORTB = 0x02;
+		break;
+
+	    case SLED_SWITCH:
+		PORTB = 0x01;
+		break;
+	}
+}
+
 int main(void) {
-    /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF; // Configure port C's 8 pins as inputs, initialize to 0s
+     	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 7 most significant pins as outputs and PB0 as input
+	unsigned char outputB = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+        while (1) {
+	// inputs:
+	
+	SwitchLED(SLED_State);
 
-    }
-    return 1;
+        // outputs:
+	PORTB = outputB;
+        } 
+  return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -26,19 +26,33 @@
 echo ======================================================\n
 echo Running all tests..."\n\n
 
-# Example test:
-test "PINA: 0x00, PINB: 0x00 => PORTC: 0"
-# Set inputs
-setPINA 0x00
-setPINB 0x00
-# Continue for several ticks
+test "PINA: 0x01 => PORTB: 0x01, state: SWITCH"
+set state = SLED_ONOF
+setPINA 0x01
 continue 2
-# Set expect values
-expectPORTC 0
-# Check pass/fail
+expectPORTB 0x01;
 checkResult
 
-# Add tests below
+
+test "PINA: 0x01, 0x00 => PORTB: 0x02, state: ONOF"
+set state = SLED_ONOF
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2 
+expectPORTB 0x02
+checkResult
+
+test "PINA: 0x01, 0x00, 0x01 => PORTB: 0x01, state: SWITCH"
+set state = SLED_ONOF
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2 
+setPINA 0x01
+continue 2
+expectPORTB 0x01
+checkResult
 
 # Report on how many tests passed/tests ran
 set $passed=$tests-$failed
